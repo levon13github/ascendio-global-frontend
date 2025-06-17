9384181079950a70d68f0f32f8e1c4d280599498
Ascendio <ascendioglobal25@gmail.com>
2025-06-17 23:18:54 +0200

    Disabled no-explicit-any rule in eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,26 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
+import { fixupConfigAsPlugin } from "@eslint/compat";
+import nextPlugin from "@next/eslint-plugin-next";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+export default [
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    languageOptions: { globals: globals.browser },
+    plugins: {
+      "@next/next": nextPlugin,
+      react: fixupConfigAsPlugin(pluginReactConfig),
+    },
+    extends: [
+      "plugin:@next/next/recommended",
+      ...tseslint.configs.recommended,
+    ],
+    rules: {
+      // Add this specific rule here:
+      "@typescript-eslint/no-explicit-any": "off"
+    },
+  },
+  // You might have other config objects in the array
 ];
-
-export default eslintConfig;
